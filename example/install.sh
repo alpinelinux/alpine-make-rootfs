0e225732f68496c55bd90b287a135c6d35445c51
Jakub Jirutka <jakub@jirutka.cz>
2018-06-27 01:04:34 +0200

    Fix typo in example/install.sh

--- example/install.sh
+++ example/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Copy some file from the repository root to the rootfs.
-install -D -m 755 examples/hello_world.rb /app/hello_world.rb
+install -D -m 755 example/hello_world.rb /app/hello_world.rb
 
 # Install some dev packages and gem mailcatcher.
 apk add --no-progress -t .make build-base ruby-dev sqlite-dev
