afabba48f31a4f65b4aaf4b45bf6ada766691fcd
Jakub Jirutka <jakub@jirutka.cz>
2025-12-04 18:04:37 +0100

    CI: Test also with Alpine v3.8

--- example/install.sh
+++ example/install.sh
@@ -6,7 +6,9 @@ install -D -m 755 hello_world.rb /app/hello_world.rb
 
 # Install some dev packages and gem mailcatcher.
 apk add --no-progress -t .make build-base ruby-dev sqlite-dev
-gem install --no-document mailcatcher
+if ruby -v | grep -q '^ruby 3.'; then  # needed for Alpine v3.8
+	gem install --no-document mailcatcher
+fi
 
 # Clean-up dev packages.
 apk del --no-progress .make
